a100dc05bf802bad65fe242e453eb3b693012360
pierrelevy <pierre.levy@paris.fr>
2017-12-05 17:19:39 +0100

    CHATBOT-4 : Add a PageInclude component to provide a chatbot popup

---
+++ src/java/fr/paris/lutece/plugins/chatbot/web/ChatPageInclude.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2002-2017, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+
+package fr.paris.lutece.plugins.chatbot.web;
+
+import fr.paris.lutece.portal.service.content.PageData;
+import fr.paris.lutece.portal.service.includes.PageInclude;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+import fr.paris.lutece.portal.service.util.AppPropertiesService;
+import fr.paris.lutece.util.html.HtmlTemplate;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * ChatPageInclude
+ */
+public class ChatPageInclude implements PageInclude
+{
+    private static final String TEMPLATE_CHAT_POPUP = "/skin/plugins/chatbot/chat_popup.html";
+    private static final String MARK_BOT_URL = "bot_url";
+    private static final String MARK_CHAT_POPUP = "chat_popup_include";
+    private static final String PROPERTY_CHAT_POPUP_BOT_URL = "chatbot.popup.bot_url";
+
+    /**
+     * {@inheritDoc }
+     */        
+    @Override
+    public void fillTemplate(Map<String, Object> rootModel, PageData data, int nMode, HttpServletRequest request)
+    {
+        Map<String, Object> model = new HashMap<>();
+        String strBotUrl = AppPropertiesService.getProperty( PROPERTY_CHAT_POPUP_BOT_URL );
+        model.put( MARK_BOT_URL , strBotUrl );
+        HtmlTemplate tChatPopup = AppTemplateService.getTemplate( TEMPLATE_CHAT_POPUP, request.getLocale() , model );
+        rootModel.put( MARK_CHAT_POPUP , tChatPopup.getHtml() );
+    }
+
+}
