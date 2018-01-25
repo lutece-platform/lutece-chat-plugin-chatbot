1c69d1363dd966f634792dcb56aed3f1b968073b
pierrelevy <pierrelevy@users.noreply.github.com>
2018-01-25 16:59:53 +0100

    CHATBOT-12 : Chatbot avatar not displayed in the list

--- src/java/fr/paris/lutece/plugins/chatbot/web/ChatBotApp.java
+++ src/java/fr/paris/lutece/plugins/chatbot/web/ChatBotApp.java
@@ -273,6 +273,8 @@ public class ChatBotApp extends MVCApplication
                 botDescription.setName( bot.getName( locale ) );
                 botDescription.setDescription( bot.getDescription( locale ) );
                 botDescription.setLanguage( locale.getDisplayLanguage( ) );
+                botDescription.setAvatarUrl( bot.getAvatarUrl( ) );
+                
                 UrlItem url = new UrlItem( URL_BOT );
                 url.addParameter( PARAMETER_BOT, bot.getKey( ) );
                 url.addParameter( PARAMETER_LANGUAGE, locale.getLanguage() );
